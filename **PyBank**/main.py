0e7789747fecf4d30e077d64de13254c3e870c53
ohoweedy <howeedy5@gmail.com>
2019-02-05 19:03:57 -0500

    update

--- **PyBank**/main.py
+++ **PyBank**/main.py
@@ -15,26 +15,41 @@ with open(pybankCSV, newline='') as csvfile:
     #print(f"CSV Header: {csv_header}")
 
     counter=0
-    sum=0
-    y=0
-    z=0
+    add=0
     profits = []
     deltas = []
+    months = []
     
     # Read each row of data after the header
     for row in csvreader:
         counter +=1
-        sum +=int(row[1])
+        add +=int(row[1])
         profits.append(int(row[1]))
+        months.append(str(row[0]))
     
-    while y < len(profits):
-        for x in profits:
-            y +=1
-            deltas = profits[y] - profits[z]
-            z +=1
-            print(deltas)
+    for x in range(1,len(profits)):
+        deltas.append(int(profits[x]) - int(profits[x-1]))
+    
+    averagechange = round((sum(deltas))/(len(deltas)), 2)
+
+max = 0
+for y in deltas:
+    if y > max:
+        max = y
+        z = deltas.index(y)
+        maxmonth = months[z+1]
+
+min = 0
+for y in deltas:
+    if y < min:
+        min = y
+        r = deltas.index(y)
+        minmonth = months[r+1]
 
 print("Financial Analysis")
 print("---------------------")
 print(f"Total Months: {counter}")
-print(f"Total: ${sum}")
+print(f"Total: ${add}")
+print(f"Average Change: ${averagechange}")
+print(f"Greatest Increase in Profits: {maxmonth} (${max})")
+print(f"Greatest Increase in Profits: {minmonth} (${min})")
